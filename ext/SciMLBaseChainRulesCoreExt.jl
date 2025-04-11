04bc604b7681348ff428414f02d3042b8685e850
DhairyaLGandhi <dhairya@juliacomputing.com>
2025-04-11 17:20:01 +0530

    chore: fix pb name

--- ext/SciMLBaseChainRulesCoreExt.jl
+++ ext/SciMLBaseChainRulesCoreExt.jl
@@ -8,10 +8,10 @@ using SymbolicIndexingInterface
 
 function Zygote.ChainRulesCore.rrule(::typeof(SciMLBase.remake), prob::ODEFunction; kw...)
     y = remake(prob; kw...)
-    function odeprob_remake_pullback(Δ)
+    function odefunc_remake_pullback(Δ)
         (Δ,)
     end
-    y, odeprob_remake_pullback
+    y, odefunc_remake_pullback
 end
 
 function Zygote.ChainRulesCore.rrule(::typeof(SciMLBase.remake), prob::ODEProblem; kw...)
